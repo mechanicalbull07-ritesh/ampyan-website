ad8966d085a73c14789ade9f2d0852c1072434ff
Ritesh Kumar <deepakdabas676@gmail.com>
2026-05-03 16:29:38 +0530

    fix production port binding

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,3 +4,5 @@ bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
 accesslog = "-"
 errorlog = "-"
 loglevel = "info"
+workers = int(os.environ.get("WEB_CONCURRENCY", "1"))
+timeout = int(os.environ.get("GUNICORN_TIMEOUT", "120"))
