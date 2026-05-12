bd091f5194bbd05ee6e0198564264e7ea6477fcd
Ritesh Kumar <deepakdabas676@gmail.com>
2026-05-12 20:15:45 +0530

    Revert "Harden Render gunicorn worker settings"
    This reverts commit 1d646e8687a933a1d2b29ac615c8f73895b32b7f.

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ echo "Starting AMPYAN website on 0.0.0.0:${PORT}"
 
 exec gunicorn app:app \
   --bind "0.0.0.0:${PORT}" \
-  --workers "${WEB_CONCURRENCY:-2}" \
-  --timeout "${GUNICORN_TIMEOUT:-60}" \
+  --workers "${WEB_CONCURRENCY:-1}" \
+  --timeout "${GUNICORN_TIMEOUT:-120}" \
   --access-logfile - \
   --error-logfile -
